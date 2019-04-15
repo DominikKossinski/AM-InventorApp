57922829cf10d347e0988fb0272b16b04e6a11ab
= <dominikkossinski@gmail.com>
2019-04-15 23:26:06 +0200

    Ios changes

--- App/App/App.iOS/AppDelegate.cs
+++ App/App/App.iOS/AppDelegate.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using CoreGraphics;
+using CoreMotion;
 using Foundation;
 using UIKit;
 
 namespace App.iOS
 {
+
+
+
+
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        UIWindow window;
+        UILabel label;
+        CMMotionManager manager;
+        UIImageView imageView;
+        UIView v;
+
+        bool counting = false;
+        int up = 0;
+        float radius = 10;
+        float x = 250;
+        float y = 250;
+
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
@@ -22,10 +39,121 @@ namespace App.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
+            window.RootViewController = new UIViewController();
+            window.MakeKeyAndVisible();
 
-            return base.FinishedLaunching(app, options);
+            v = new UIView(window.Bounds);
+            v.BackgroundColor = UIColor.White;
+
+            label = new UILabel(new CGRect(50, 50, 200, 21));
+            label.Text = "Abc";
+            label.TextColor = UIColor.Black;
+
+
+
+            manager = new CMMotionManager();
+            if (manager.AccelerometerAvailable)
+            {
+                label.Text = "Available";
+                if (manager.AccelerometerActive)
+                {
+                    label.Text = "Active";
+                }
+                manager.AccelerometerUpdateInterval = 0.01;
+                manager.StartAccelerometerUpdates(NSOperationQueue.CurrentQueue,
+                    new CMAccelerometerHandler(calculate));
+            }
+            else
+            {
+                UIView circle = new UIView(new CGRect(250, 250, 2 * radius, 2 * radius));
+                circle.Layer.BorderWidth = 1;
+                circle.BackgroundColor = UIColor.Black;
+                v.AddSubview(circle);
+                label.Text = "No accelerometer";
+                /*for (int i = 0; i < 50; i++)
+                {
+                    circle = new UIView(new CGRect(50, 100 + i * 2, 2 * radius, 2 * radius));
+                    circle.Layer.BorderWidth = 1;
+                    circle.BackgroundColor = UIColor.Black;
+                    v.AddSubview(circle);
+                }*/
+            }
+            v.AddSubview(label);
+
+            imageView = new UIImageView(new CGRect(0, 50, 500, 500));
+
+
+            window.AddSubview(v);
+
+
+
+            return true;
         }
+
+        private void calculate(CMAccelerometerData data, NSError error)
+        {
+            label.Text = string.Format("x={0:f}, y={1:f}, z={2:f}", data.Acceleration.X,
+                data.Acceleration.Y, data.Acceleration.Z);
+            x -= (float)data.Acceleration.X;
+            y += (float)data.Acceleration.Y;
+            if (x > 500)
+            {
+                x = 500;
+            }
+            if (x < 0)
+            {
+                x = 0;
+            }
+            if (y > 500)
+            {
+                y = 500;
+            }
+            if (y < 0)
+            {
+                y = 0;
+            }
+            if (data.Acceleration.Z - 9.8 < -3)
+            {
+                if (counting)
+                {
+                    if (up == 0)
+                    {
+                        up = 5;
+                    }
+                }
+                else
+                {
+                    up = 5;
+
+                }
+
+            }
+            if (data.Acceleration.Z - 9.8 > 3.5)
+            {
+                if (counting)
+                {
+                    up = -5;
+                }
+                else
+                {
+                    up = -5;
+
+                }
+            }
+            if (radius > 30)
+            {
+                radius = 30;
+            }
+            if (radius < 5)
+            {
+                radius = 5;
+            }
+            UIView circle = new UIView(new CGRect(x, y, 2 * radius, 2 * radius));
+            circle.Layer.BorderWidth = 1;
+            circle.BackgroundColor = UIColor.Black;
+            v.AddSubview(circle);
+        }
+
     }
 }
